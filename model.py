716d3db370cdc7edc535adc641d93d2367a805ee
davutcagri <davutcagria@gmail.com>
2025-01-30 13:02:22 +0300

    Update 2

--- model.py
+++ model.py
@@ -8,34 +8,35 @@ from sklearn.metrics import classification_report
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
-path = 'datasets/train.csv'
-
-df = pd.read_csv(path)
-
-df = df[['text', 'label']] # Sadece text ve label sütunlarını seç
-df['cleaned_text'] = df['text'].apply(preview.clean) # cleaned_text sütunu oluştur ve clean metodundan geçmiş text datalarını sütuna yaz
-
 model = LogisticRegression()
 tfidf = TfidfVectorizer()
 
+train_path = 'datasets/train.csv'
+valid_path = 'datasets/valid.csv'
+
+df = pd.read_csv(train_path)
+df = df[['text', 'label']] # Sadece text ve label sütunlarını seç
+df['cleaned_text'] = df['text'].apply(preview.clean) # Ön temizlikten geçir
+
 # TF-IDF Vektörleştirme
-X = tfidf.fit_transform(df['cleaned_text'])
-Y = df['label']
+X_train = tfidf.fit_transform(df['cleaned_text'])
+Y_train = df['label']
 
 #Tüm veriyi kullanarak modeli eğitmek
-model.fit(X, Y)
+model.fit(X_train, Y_train)
 
 def classification_test():
     test_df = pd.read_csv('datasets/test.csv')
+
     X_test = tfidf.transform(test_df['text'])
     Y_test = test_df['label']
 
-    y_pred = model.predict(X_test)
+    Y_pred = model.predict(X_test)
 
-    return classification_report(y_pred, Y_test)
+    return classification_report(Y_pred, Y_test)
 
 def input_filter(input_text):
     X_input = tfidf.transform([input_text])
-    prediction = model.predict(X_input)
+    Y_pred = model.predict(X_input)
 
-    return prediction
+    return Y_pred
